fa2d784175442f25fc3e9462cb6c4bf956be5b9c
Patrik Bóna <patrik.bona@mrhead.sk>
2016-03-14 00:10:32 +0100

    Post to the root URL
    So if someone hit refresh after unsuccessful form validation we do not
    fail because of non-existing GET URL (`/votes`).

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "votes#new"
 
-  resource :votes, only: :create
+  post "/" => "votes#create", as: :votes
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
