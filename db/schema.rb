27cd4feaccd4999a660fae7b9fa65c73c15c0456
Patrik Bóna <patrik.bona@mrhead.sk>
2016-03-14 01:18:45 +0100

    Change default value for vote#public to true

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313224810) do
+ActiveRecord::Schema.define(version: 20160314001732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 20160313224810) do
     t.string   "name"
     t.string   "city"
     t.string   "email"
-    t.boolean  "public"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "public",     default: true
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
